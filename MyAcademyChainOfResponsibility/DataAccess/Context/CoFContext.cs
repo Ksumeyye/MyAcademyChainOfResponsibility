44387bee609777ef4e0d3e18ece059f9dc385952
ksumeyye <sumeyyeekaya@outlook.com>
2024-10-10 01:08:38 +0300

    Add project files.

---
+++ MyAcademyChainOfResponsibility/DataAccess/Context/CoFContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAcademyChainOfResponsibility.DataAccess.Entities;
+
+namespace MyAcademyChainOfResponsibility.DataAccess.Context
+{
+	public class CoFContext:DbContext
+	{
+        public CoFContext(DbContextOptions options):base(options) // base ifadesi, yapılan temel ayarlamaları içerisine almak için kullanılır
+        {
+            
+        }
+        public DbSet<CustomerProcess>CustomerProcesses { get; set; }
+    }
+}
